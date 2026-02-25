0bf284e65035055ee3160e5f9a12ade290ba2c88
dmitriyneto <qima10021981@gmail.com>
2026-02-24 18:40:02 -0800

    First commit

--- src/test/java/ru/netology/selenium/CardOrderTest.java
+++ src/test/java/ru/netology/selenium/CardOrderTest.java
@@ -1,13 +1,10 @@
 package ru.netology.selenium;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CardOrderTest {
     private WebDriver driver;
